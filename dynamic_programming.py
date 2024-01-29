826359225db15e1f2d25248f23491d6d7ac193de
Emerald <emerald.henry@stu.cu.edu.ng>
2024-01-29 20:39:03 +0100

    updates

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -36,3 +36,37 @@ class Solution:
     
 
 
+"""Question 64 Minimum Path Sum
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+"""
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+            
+        
+        m, n = len(grid), len(grid[0])
+        
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[-1][-1]
+
+"""Intuition
+-The intuition behind the dynamic programming approach is that the minimum path sum to reach a position (i, j) in the grid can be computed by considering the minimum path sum to reach the positions (i-1, j) and (i, j-1).
+
+-This is because the only two possible ways to reach the position (i, j) are either by moving down from (i-1, j) or moving right from (i, j-1).
+
+-By computing the minimum path sum to reach each position in the grid, the algorithm can find the minimum path sum to reach the bottom-right corner of the grid by simply looking at the last element of the array (grid[m-1][n-1]).
+
+"""
