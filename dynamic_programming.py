88e593e75fcb4c5bf99ad29ac74c240aeaf8e4e8
Emerald <emerald.henry@stu.cu.edu.ng>
2024-02-05 21:09:39 +0100

    updates

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -70,3 +70,23 @@ class Solution:
 -By computing the minimum path sum to reach each position in the grid, the algorithm can find the minimum path sum to reach the bottom-right corner of the grid by simply looking at the last element of the array (grid[m-1][n-1]).
 
 """
+"""Question 62. Unique Paths
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+Input: m = 3, n = 7
+Output: 28
+"""
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1]*n
+
+        for i in range(m-1):
+            newRow = [1]*n
+
+            for j in range(n-2, -1, -1):
+                newRow[j] = newRow[j+1] + row[j]
+            row = newRow 
+
+        return row[0]
