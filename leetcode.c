b384651c083e9ceb137c12ee3adb76d70b7eb6da
emerald <emeraldhenry3@gmail.com>
2025-04-22 22:54:59 +0100

    additions

--- leetcode.c
+++ leetcode.c
@@ -345,3 +345,39 @@ int strStr(char* haystack, char* needle) {
     return -1;
 
 }
+
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+*/
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize-1;
+
+    while (left <= right){
+        int mid = (left + right) / 2;
+
+        if (nums[mid] == target){
+            return mid;
+        } else if (nums[mid] < target){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
